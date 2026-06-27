cdb504f695bea6c8369837ecfc04f3b6bc2fec4d
lafuan <lafuan@users.noreply.github.com>
2026-06-27 08:18:19 +0700

    fix: mypy type annotation in merge migration

--- backend/migrations/versions/79071da84543_merge_subscription_and_parent_.py
+++ backend/migrations/versions/79071da84543_merge_subscription_and_parent_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '79071da84543'
-down_revision: Union[str, None] = ('b4a7f2e9c8d1', 'f7e3d8a2b1c4')
+down_revision: Union[str, tuple[str, str], None] = ('b4a7f2e9c8d1', 'f7e3d8a2b1c4')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
